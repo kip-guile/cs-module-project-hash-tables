42115551d29a933ef997371e0d1c121cb4a7023f
Alexander Oguejiofor <oguejioforalexander@gmail.com>
2020-07-08 23:25:31 +0100

    word count

--- applications/word_count/word_count.py
+++ applications/word_count/word_count.py
@@ -1,10 +1,30 @@
 def word_count(s):
     # Your code here
+    lowercase = s.lower()
+    whitespace = '\n \t \r'.split()
+    # whitespace_to_space
+    for i in whitespace:
+        lowercase = lowercase.replace(i, ' ')
+    chars = '": ; , . - + = / \ | [ ] { } ( ) * ^ &'.split(' ')
+    # remove_chars
+    for i in chars:
+        lowercase = lowercase.replace(i, '')
+    strings = lowercase.split(' ')
+    seen = {}
+    for string in strings:
+        if string == '':
+            continue
+        if string in seen:
+            seen[string] += 1
+        else:
+            seen[string] = 1
 
+    return seen
 
 
 if __name__ == "__main__":
     print(word_count(""))
     print(word_count("Hello"))
     print(word_count('Hello, my cat. And my cat doesn\'t say "hello" back.'))
-    print(word_count('This is a test of the emergency broadcast network. This is only a test.'))
+    print(word_count(
+        'This is a test of the emergency broadcast network. This is only a test.'))
